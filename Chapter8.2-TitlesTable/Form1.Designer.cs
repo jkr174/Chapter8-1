79eb5d5c827e1d29c1ad5c2655bd61af025bf58f
jkr174 <romo17410@gmail.com>
2021-03-18 20:34:40 -0500

    Chapter8.2 Titles Table

---
+++ Chapter8.2-TitlesTable/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace Chapter8._2_TitlesTable
+{
+    partial class frmTitles
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grdTitles = new System.Windows.Forms.DataGridView();
+            this.dlgOpen = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.grdTitles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grdTitles
+            // 
+            this.grdTitles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdTitles.Location = new System.Drawing.Point(12, 12);
+            this.grdTitles.Name = "grdTitles";
+            this.grdTitles.Size = new System.Drawing.Size(445, 274);
+            this.grdTitles.TabIndex = 0;
+            // 
+            // dlgOpen
+            // 
+            this.dlgOpen.FileName = "[blank]";
+            this.dlgOpen.Filter = "SQL Server Databases (*.mdf)|*.mdf";
+            // 
+            // frmTitles
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 299);
+            this.Controls.Add(this.grdTitles);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmTitles";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Titles Table";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmTitles_FormClosing);
+            this.Load += new System.EventHandler(this.frmTitles_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grdTitles)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grdTitles;
+        private System.Windows.Forms.OpenFileDialog dlgOpen;
+    }
+}
+
---
+++ Chapter8.2-TitlesTable/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter8._2_TitlesTable
+{
+    public partial class frmTitles : Form
+    {
+        SqlConnection booksConnection; 
+        SqlCommand titlesCommand; 
+        SqlDataAdapter titlesAdapter; 
+        DataTable titlesTable;
+
+        bool selectedFile = true;
+        public frmTitles()
+        {
+            InitializeComponent();
+        }
+
+        private void frmTitles_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (selectedFile != false)
+            {
+                booksConnection.Dispose();
+                titlesCommand.Dispose();
+                titlesAdapter.Dispose();
+                titlesTable.Dispose();
+            }
+        }
+
+        private void frmTitles_Load(object sender, EventArgs e)
+        {
+            if (dlgOpen.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // connect to books database 
+                    booksConnection = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;" +
+                            "AttachDbFilename=" + dlgOpen.FileName + "; " +
+                            "Integrated Security=True;" +
+                            "Connect Timeout=30;" +
+                            "User Instance=False");
+
+                    booksConnection.Open(); 
+                    // establish command object 
+                    titlesCommand = new SqlCommand(
+                        "Select * " +
+                        "from Titles " +
+                        "ORDER BY Title", booksConnection); 
+                    // establish data adapter/data table 
+                    titlesAdapter = new SqlDataAdapter(); 
+                    titlesAdapter.SelectCommand = titlesCommand; 
+                    titlesTable = new DataTable(); 
+                    titlesAdapter.Fill(titlesTable); 
+                    // bind grid to data table 
+                    grdTitles.DataSource = titlesTable; 
+                } 
+                catch (Exception ex) 
+                {
+                    MessageBox.Show(
+                        ex.Message, 
+                        "Error establishing database file connection.", 
+                        MessageBoxButtons.OK, 
+                        MessageBoxIcon.Error); 
+                    return; 
+                } 
+            } 
+            else 
+            {
+                selectedFile = false;
+                MessageBox.Show(
+                    "No file selected", 
+                    "Program stopping",
+                    MessageBoxButtons.OK, 
+                    MessageBoxIcon.Information); 
+                this.Close(); 
+            }
+        }
+    }
+}
